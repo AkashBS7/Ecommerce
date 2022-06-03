a64cd5998bdf0b77fd4199ec8b16f57730218f6d
Akash Behara <akashbehara33@gmail.com>
2022-06-03 20:41:50 +0530

    homepage UI/api, sanity connect

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,35 @@
 import React from 'react'
 import { Product, FooterBanner, HeroBanner } from '../components'
 
-const Home = () => {
+import { client } from '../lib/client'
+
+const Home = ({ products, bannerData }) => {
   return (
     <>
-      <HeroBanner />
+      {console.log('banner', bannerData)}
+      <HeroBanner heroBanner={bannerData.length && bannerData[0]}/>
       <div className='products-heading'>
         <h2>Best Selling Products</h2>
         <p>Speakers of many variations</p>
       </div>
       <div className='products-container'>
-          {['Produtc1','product2','product3','product4','product5'].map((el) => el)}
+          {products?.map((el) => el.name)}
       </div>
       <FooterBanner />
     </>
   )
 }
+
+export const getServerSideProps = async () => {
+  const query = `*[_type == "product"]`;
+  const products = await client.fetch(query);
+
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: { products, bannerData }
+  }
+};
 
 export default Home
